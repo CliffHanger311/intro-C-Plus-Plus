d7e1314043678e01ab6b007656d69ad1204abe56
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-09-10 14:01:58 -0500

    Assignment02cout

---
+++ Assignment02cout/Assignment02cout/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	//Display strings to user
+	cout << "Computers, computers everywhere\n";
+	cout << "  as far as I can C.\n";
+	cout << "I really, really like these things.\n";
+	cout << "  Oh joy. Oh joy for me!";
+
+	//Pause program to show output, user input continues program
+	_getch();
+	return 0;
+}
