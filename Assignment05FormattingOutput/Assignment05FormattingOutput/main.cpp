6122c9877a2227774fbace992b543964d8186fa9
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-09-19 02:24:15 -0500

    Assignment 5 - Formatting Output, completed

--- Assignment05FormattingOutput/Assignment05FormattingOutput/main.cpp
+++ Assignment05FormattingOutput/Assignment05FormattingOutput/main.cpp
@@ -1,3 +1,9 @@
+/*
+	Author: CliffHanger
+	C++, Assignment 5 - Formatting Outputs
+	9/19/12
+*/
+
 #include <iostream>
 #include <conio.h>
 #include <iomanip>
@@ -8,24 +14,41 @@ int main()
 {
 	//declare variable for calcultions
 	int intNumber = 32;
-	double dblnumber = 42.98765;
+	float fltnumber = 42.98765;
+	int ColumnWidth = 10;
 
 	//Display to the user the hx and octal values
-	cout << "The hex of 32 is " << endl;
-	cout << "The octal of 32 is " << endl;
+	cout << "The hex of 32 is " << showbase << hex << intNumber << endl;
+	cout << "The octal of 32 is " << showbase << oct << intNumber << endl;
 	cout << endl;
 
-	//Display the doubles with fixed precision, each line is a different decimal place, with proper formatting
-	cout << "|" << dblnumber << "|" << endl;
-	cout << "|" << dblnumber << "|" << endl;
-	cout << "|" << dblnumber << "|" << endl;
-	cout << "|" << dblnumber << "|" << endl;
-	cout << endl;
+	//Display the doubles with fixed precision, staring with three, then 4, 5, and 6, each on a newline
+	cout << fixed << setprecision(3);
+	cout << "|" << right << setw(ColumnWidth) << fltnumber << "|" << endl;
+	cout << fixed << setprecision(4);
+	cout << "|" << right << setw(ColumnWidth) << fltnumber << "|" << endl;
+	cout << fixed << setprecision(5);
+	cout << "|" << right << setw(ColumnWidth) << fltnumber << "|" << endl;
+	cout << fixed << setprecision(6);
+	cout << "|" << right << setw(ColumnWidth) << fltnumber << "|" << endl;
+	cout << endl; //Space between outputs
 
-	//display the finally values in one line, after a space
-
+	//Set column size for each cell, no endl
+	ColumnWidth = 5;
+	cout << "|" << setw(ColumnWidth) << showbase << dec << intNumber << "|";
+	ColumnWidth = 10;
+	//set fltnumber to 2 decimal places
+	cout << fixed << setprecision(2);
+	cout << setw(ColumnWidth) << fltnumber << "|";
 
 	_getch();
 	return 0;
+
+
+	/*
+		I didn't see any difference in running the program.
+		Float and doubles are similar data types, but the differences 
+		is doubles hold a greater range of values with there increased byte count.
+	*/
 }
 
