b4b0e3d4cc585e6e8ec925f0b2c93e16405945d2
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-10-01 13:45:01 -0500

    10/1/2012 class demos, made corrections for assignment 7

--- Assingment07Salary/Assingment07Salary/main.cpp
+++ Assingment07Salary/Assingment07Salary/main.cpp
@@ -6,57 +6,107 @@ using namespace std;
 
 int main()
 {
-Start:
-	//Declare variables for variables
-	double Pay = 8.00;
-	double OTPay = 12.00;
-	double Hours = 0;
-	double Overtime = 0;
-	double Salary = 0;
+	bool Continue = true;
 
-	//Display message asking for input of hours, put input to hours variable
-	cout << "Enter number of hours worked?" << endl;
-	cin >> Hours;
-
-	//Check to make sure input is >= 0
-InputCheck:
-	if (Hours < 0)
+	//Calculate Hours until user chooses to stop
+	while (Continue == true)
 	{
-		//Display error, and ask to try again
-		cout << "Error: Input needs to be 0 or greater" << endl
-			<< endl
-			<< "Please enter a different value" << endl;
+		//Declare variables for variables
+		double Pay = 8.00;
+		double OTPay = 12.00;
+		double Hours = 0;
+		double Overtime = 0;
+		double Salary = 0;
+		char C;
 
-		//Put new input into hours variables
+		//Display message asking for input of hours, put input to hours variable
+		cout << "Enter number of hours worked?" << endl;
 		cin >> Hours;
-		goto InputCheck;
-	}
 
-	if (Hours < 40)
-	{
-		//Set Display to two decimal places
-		cout << fixed << setprecision(2);
+		//Check to make sure input is >= 0
+		while(Hours < 0)
+		{
+			//Display error, and ask to try again
+			cout << "Error: Input needs to be 0 or greater" << endl
+				<< endl
+				<< "Please enter a different value" << endl;
 
-		//Calculate Hours with Pay for salary, display answer
-		Salary = Hours * Pay;
-		cout << "Your Salary is " << Salary << endl;
-	}
-	else 
-	{
-		//Set Display to two decimal places
-		cout << fixed << setprecision(2);
+			//Put new input into hours variables
+			cin >> Hours;
+		}
 
-		//Seperate hours over 40
-		Overtime = Hours - 40;
+		if (Hours < 40)
+		{
+			//Set Display to two decimal places
+			cout << fixed << setprecision(2);
 
-		//Subtract Overtime from Hours for hours to be $40.00
-		Hours = Hours - Overtime;
+			//Calculate Hours with Pay for salary, display answer
+			Salary = Hours * Pay;
+			cout << "Your Salary is " << Salary << endl;
+		}
+		else 
+		{
+			//Set Display to two decimal places
+			cout << fixed << setprecision(2);
+
+			//Seperate hours over 40
+			Overtime = Hours - 40;
+
+			//Subtract Overtime from Hours for hours to be $40.00
+			Hours = Hours - Overtime;
 		
-		//Calculate Hours with Pay for salary(Splus 4 for overtime), display answer
-		Salary = Hours * (Pay);
-		Salary += Overtime * (OTPay);
-		cout << "Your Salary is " << Salary << endl;
-	}
+			//Calculate Hours with Pay for salary(Splus 4 for overtime), display answer
+			Salary = Hours * (Pay);
+			Salary += Overtime * (OTPay);
+			cout << "Your Salary is " << Salary << endl;
+		}
+
+		//Request for user to continue or not
+		cout << "So you wish to calculate another salary? y/n" << endl;
+		cin >> C;
+		
+		//Invalid input, loop until user inputs correctly
+			if (C == 'y')
+				{
+					//Set Continue to true
+					continue;
+				}
+				else if(C == 'n')
+				{
+					//Continue to end
+					Continue = false;
+					break;
+				}
+				else 
+				{
+					//Error happened with continue input
+					bool Error = true;
+					while (Error == true)
+					{
+						//Ask user to try again
+						cout << "Please try again! Enter y for yes, or n for no: " << endl;
+						cin >> C;
+
+						if (C == 'y')
+						{
+							//Error corrected, continue yes
+							Error = false;
+							Continue = true;
+						}
+						else if (C == 'n')
+						{
+							//Error corrected, continue no
+							Error = false;
+							Continue = false;
+						}
+						else
+						{
+							//Another error, repeat error handle
+							Error = true;
+						}
+					}
+				}		
+		}
 
 	_getch();
 	return 0;
