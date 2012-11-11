bb1cfcc37ea5011d5c95b582543a24f939abf71f
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-11-11 01:15:49 -0600

    Revert "Assignment15Struct, Fnished With Extra Credit"
    This reverts commit 7a028e7bf6cf10bfe613ac849af62276fe861e18.

--- Assignment15Struct/Assignment15Struct/main.cpp
+++ Assignment15Struct/Assignment15Struct/main.cpp
@@ -15,9 +15,8 @@ struct Employee
 
 int main()
 {
-	//Variables to hold default values
-	//Employee myEmployees[10];
-	
+	//Variables to hold values
+	Employee myEmployees[10];
 	//int EmployeeLength = 10;
 	int EmployeeIndex = 0;
 	char c = '0';
@@ -28,22 +27,9 @@ int main()
 		<< "You may list any number of employees you have, " << endl
 		<< "and you can quit whenever you need to." << endl << endl;
 
-	//declare for array length
-	  int arrLength;
-	  cout << "How many employees will you be entering? ";
-	  cin >> arrLength;
-	  cout << endl;
-
-	//declare int pointer AND allocate new integer array of arrLength
-	Employee *myEmployees = new Employee[arrLength];
-
-	//For loop to enter employees Information
-	for (int c = 1; c <= arrLength; c++)
+	while (!isQuit)
 	{
 		Employee TempEmployee;
-
-		//Output which number is being input
-		cout << c << " Employee" << endl;
 
 		//Ask for input for Number, collect in structure
 		cout << "Number: " ;
@@ -60,50 +46,48 @@ int main()
 		//Ask for input for Hour, collect in structure
 		cout << "Hour: ";
 		cin >> TempEmployee.Hour;
-		cout << endl;
 
 		//Set new employee to the array
 		myEmployees[EmployeeIndex] = TempEmployee;
 		EmployeeIndex++;
-	}
 
-		//Output titles of columns
-		const int ColumnLenghtNumber = 8;
-		const int ColumnLenghtName = 15;
-		const int ColumnLenghtHrR = 8;
-
-		cout << endl << fixed << setprecision(2);
-		cout << left << setw(ColumnLenghtNumber) << "Number"
-			 << left << setw(ColumnLenghtName) << "Name"
-			 << left << setw(ColumnLenghtHrR) << "Rate" << "\t"
-			 << left << setw(ColumnLenghtHrR) << "Hour" << endl;
+		//Out titles of columns
+		cout << fixed << setprecision(2);
+		cout << left << setw(10) << "Number"
+			 << left << setw(10) << "Name"
+			 << left << setw(10) << "Rate"
+			 << left << setw(10) << "Hour" << endl;
 
 		for (int i = 0; i < EmployeeIndex; i++)
 			{
 				//Output the values of each row
-				cout << left << setw(ColumnLenghtNumber) << myEmployees[i].Number
-					 << left << setw(ColumnLenghtName)<< myEmployees[i].Name
-					 << right << setw(ColumnLenghtHrR) << myEmployees[i].Rate << "\t"
-					 << right << setw(ColumnLenghtHrR) << myEmployees[i].Hour
+				cout << left << setw(10) << myEmployees[i].Number
+					 << left << setw(10)<< myEmployees[i].Name
+					 << right << setw(10) << myEmployees[i].Rate
+					 << right << setw(10) << myEmployees[i].Hour
 					 << endl;
 			}
 
-		////Ask the user if they wish to continue
-		//cout << "Press q or Q to quit, any button to continue: ";
-		//cin >> c;
-		//cout << endl;
+		//Ask the user if they wish to continue
+		cout << "Press q or Q to quit, any button to continue: ";
+		cin >> c;
+		cout << endl;
 
-		//if (c == 'q' || c == 'Q')
-		//{
-		//	//Set to true, user choose to quit
-		//	isQuit = true;
-		//}
-		//else
-		//{
-		//	//Set to false, user wishes to continue
-		//	isQuit = false;
-		//}
+		if (c == 'q' || c == 'Q')
+		{
+			//Set to true, user choose to quit
+			isQuit = true;
+		}
+		else
+		{
+			//Set to false, user wishes to continue
+			isQuit = false;
+		}
+	}
 
 	_getch();
 	return 0;
 }
+
+
+		//cin.ignore(256, '\n');
