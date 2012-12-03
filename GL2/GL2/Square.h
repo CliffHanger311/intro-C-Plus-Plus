fc601356ab4df120882db76f817eff8e69806061
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-12-03 14:00:16 -0600

    GL2, more animations

---
+++ GL2/GL2/Square.h
@@ -0,0 +1,24 @@
+#include <glut.h>
+
+
+
+
+class square
+{
+private:
+	GLfloat _width;
+	bool _isVisable;
+	GLfloat x,y;
+	GLfloat *_color;
+	
+
+public:
+	square();
+	square(GLfloat,GLfloat*);
+	void show();
+	void hide();
+	void draw();
+	void move(GLfloat, GLfloat);
+	void setColor(GLfloat*);
+
+};
---
+++ GL2/GL2/square.cpp
@@ -0,0 +1,72 @@
+#include "Square.h"
+
+//GLfloat _width;
+
+
+//square();
+square::square()
+{
+	_width = 5.0f;
+	_isVisable = true;
+	x = 0;
+	y = 0;
+}
+
+//square(GLfloat);
+square::square(GLfloat width, GLfloat * color)
+{
+	_width = width;
+	_isVisable = true;
+	x = 0;
+	y = 0;
+
+	_color = new GLfloat[4];
+	_color[0] = color[0];
+	_color[1] = color[1];
+	_color[2] = color[2];
+	_color[3] = color[3];
+}
+
+//void show();
+void square::show()
+{
+	_isVisable = true;
+}
+
+//void hide();
+void square::hide()
+{
+	_isVisable = false;
+}
+
+void square::draw()
+{
+	if (_isVisable)
+	{
+	//Code for color blue
+	static GLfloat blue[]    = {0.0, 0.0, 1.0, 1.0};
+
+	//Code to draw square
+	glBegin(GL_QUADS);
+       glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, _color);
+	   glVertex3f(-_width/2 + x,_width/2 + y,0);
+	   glVertex3f(_width/2 + x,_width/2 + y,0);
+       glVertex3f(_width/2 + x,-_width/2 + y,0);
+	   glVertex3f(-_width/2 + x,-_width/2 + y,0);
+   glEnd();
+	}
+}
+
+void square::move(GLfloat move_x, GLfloat move_y)
+{
+	x += move_x;
+	y += move_y;
+}
+
+void square::setColor(GLfloat* color)
+{
+	_color[0] = color[0];
+	_color[1] = color[1];
+	_color[2] = color[2];
+	_color[3] = color[3];
+}
