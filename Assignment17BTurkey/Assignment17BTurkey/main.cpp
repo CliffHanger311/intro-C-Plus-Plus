3d9de3cc889233374ded8cb2d3d3cc924bb105ef
Clifford Duda <cliffhangerthefirst@gmail.com>
2012-12-13 16:33:29 -0600

    Assignment17BTurkey
    Close to finishing, need to do the Turkey ASCII yet.
    Also troubleShoot

--- Assignment17BTurkey/Assignment17BTurkey/main.cpp
+++ Assignment17BTurkey/Assignment17BTurkey/main.cpp
@@ -2,12 +2,72 @@
 
 using namespace std;
 
+//Function Prototypes
+bool Continue(char);
+
+//Global Variables
+bool blnContinue = false;
+
+
 int main()
 {
+	//Variable to hold input
+	char userInput = 0;
+	int TurkeySize = 0;
 
+	//Display greetings to the user
+	cout << "Happy Thanksgiving!!" << endl;
 
+	do
+	{
+		//Ask user what size of turkey they would like
+		cout << "How large of a turkey are you looking for?" << endl
+			<< "Please enter a number: ";
+		cin >> TurkeySize;
 
+		//Turkey ASCII code here
+		cout << TurkeySize << endl;
+
+		//Ask user if they wish to continue
+		cout << "Is this the right size of turkey for you, or should we try another?" << endl;
+		cout << "Please enter a Y for Yes, or N for No: ";
+		cin >> userInput;
+
+		blnContinue = Continue(userInput);
+	}while (blnContinue == true);
+
+	cout << endl << "Thank you for visiting! Enjoy your Holidays!!" << endl;
 
 	_getch();
 	return 0;
+}
+
+bool Continue(char Input)
+{
+	//bool to control 
+	bool ValidateInput = false;
+
+	//Do loop to control input validation
+	do
+	{
+		if (Input == 'y' || Input == 'Y')
+			{
+				//Return true and Validate true
+				ValidateInput = true;
+				return true;
+			}
+			else if (Input == 'n' || Input == 'N')
+			{
+				//Return false and Validate true
+				ValidateInput = true;
+				return false;
+			}
+			else
+			{
+				//return nothing and Validate False
+				ValidateInput = false;
+				cout << "Please enter Y for Yes, or N for No: ";
+				cin >> Input;
+			}
+	} while (ValidateInput == false);
 }
